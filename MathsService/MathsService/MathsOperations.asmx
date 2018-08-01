694cd603530da6a4e4d9e3bce98a6590f617215e
Masupha Letsie <tanzupa77@outlook.com>
2018-08-01 22:54:45 +0200

    Add function

---
+++ MathsService/MathsService/MathsOperations.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="MathsOperations.asmx.cs" Class="MathsService.MathsOperations" %>
---
+++ MathsService/MathsService/MathsOperations.asmx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace MathsService
+{
+    /// <summary>
+    /// Summary description for MathsOperations
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class MathsOperations : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public decimal Add(decimal a, decimal b)
+        {
+            return a+b;
+        }
+    }
+}
